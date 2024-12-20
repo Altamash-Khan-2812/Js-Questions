97695f2d864e77ffbb347e5a2538323a26c8678f
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2024-12-20 23:43:32 +0530

    Solved even odd question with three ways

---
+++ EvenOdd/even.js
@@ -0,0 +1,23 @@
+// <!-- 1. Even or Odd: -->
+// <!-- Write a program that takes an integer input and prints whether it's even or odd. -->
+
+// function isEven(int) {
+// if (int % 2 === 0) {
+//   return `${int} is an even number`;
+// } else {
+//   return `${int} is an odd number`;
+// }
+// }
+
+// function isEven(num) {
+//   return num % 2 === 0 ? true : false;
+// }
+
+// function isEven(num) {
+//   switch (num % 2) {
+//     case 0:
+//       return true;
+//     default:
+//       return false;
+//   }
+// }
