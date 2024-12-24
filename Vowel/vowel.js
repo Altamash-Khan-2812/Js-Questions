024f50d9bfcde50952e77ceaef1aad839b729e0e
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2024-12-24 21:13:37 +0530

    Solved vowel consonant question in two ways

---
+++ Vowel/vowel.js
@@ -0,0 +1,26 @@
+// 6. Vowel or Consonant:
+//  Write a program that checks if a given alphabet is a vowel or a consonant.
+// function isVowel(alph) {
+//   let alphabet = alph.toLowerCase();
+//   if (
+//     alphabet === "a" ||
+//     alphabet === "e" ||
+//     alphabet === "i" ||
+//     alphabet === "o" ||
+//     alphabet === "u"
+//   ) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// function isVowel(alphabet) {
+//   let vowels = ["a", "e", "i", "o", "u"];
+//   for (let i = 0; i < vowels.length; i++) {
+//     if (alphabet === vowels[i]) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
