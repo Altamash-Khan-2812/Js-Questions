01d12fefb2437a7843f81827f6ed1bd1f3bf0098
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2024-12-27 18:42:50 +0530

    Solved the emailValidator using a regex patter

---
+++ emailValid/email.js
@@ -0,0 +1,10 @@
+// Check if a given string is a valid email address.
+
+// function checkEmail(str) {
+//   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+//   if (emailRegex.test(str)) {
+//     return `${str} is a valid email address`;
+//   } else {
+//     return `${str} is not a vaid email address`;
+//   }
+// }
