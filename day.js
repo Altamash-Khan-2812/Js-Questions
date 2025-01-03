6f1fb35b8e71d353e89f562ca84d4ac1de161f55
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2025-01-03 20:20:38 +0530

    Solved day of week question using if condition and using values from an object

---
+++ day.js
@@ -0,0 +1,35 @@
+// Write a program that takes a number (1 to 7) as input and prints the corresponding day of the week.
+
+// function dayOfWeek(day) {
+//   if (day === 1) {
+//     return "Monday";
+//   } else if (day === 2) {
+//     return "Tuesday";
+//   } else if (day === 3) {
+//     return "Wednesday";
+//   } else if (day === 4) {
+//     return "Thursday";
+//   } else if (day === 5) {
+//     return "Friday";
+//   } else if (day === 6) {
+//     return "Saturday";
+//   } else if (day === 7) {
+//     return "Sunday";
+//   } else {
+//     return "Invalid day";
+//   }
+// }
+
+// let days = {
+//   1: "Monday",
+//   2: "Tuesday",
+//   3: "Wednesday",
+//   4: "Thursday",
+//   5: "Friday",
+//   6: "Saturday",
+//   7: "Sunday",
+// };
+
+// function dayOfWeek(day) {
+//   return days[day];
+// }
