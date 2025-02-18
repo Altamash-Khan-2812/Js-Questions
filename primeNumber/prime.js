c02f55b3415e1ffffafc694208fb69b76b5a9642
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2025-02-18 14:47:43 +0530

    Update isPrime function using getFactors

--- primeNumber/prime.js
+++ primeNumber/prime.js
@@ -1,12 +1,32 @@
 // Write a function to check if a number is prime.
 
-function isPrime(num) {
-  let halfOfNum = Math.floor(num / 2);
+// function isPrime(num) {
+//   let halfOfNum = Math.floor(num / 2);
 
-  for (let i = 2; i < halfOfNum; i++) {
+//   for (let i = 2; i < halfOfNum; i++) {
+//     if (num % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+function getFactors(num) {
+  let factorsArr = [];
+  for (let i = 1; i <= num; i++) {
     if (num % i === 0) {
-      return false;
+      factorsArr.push(i);
     }
   }
-  return true;
+  return factorsArr;
+}
+
+function isPrime(num) {
+  if (num <= 1) {
+    return "number should be greater than 1";
+  } else if (getFactors(num).length === 2) {
+    return true;
+  } else {
+    return false;
+  }
 }
